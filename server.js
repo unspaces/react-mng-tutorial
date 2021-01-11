3c1f5b2158eadadbafe96db6c0d2e5b4d9fd59a3
unspaces <unspaces@naver.com>
2021-01-11 17:32:53 +0900

    Add Get Customers REST API Module

--- server.js
+++ server.js
@@ -6,9 +6,33 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
-app.get('/api/hello', (req, res) => {
-    res.send({message: 'Hello Express!'});
-
+app.get('/api/customers',(req,res) => {
+  res.send([
+    {
+      'id': 1,
+      'image': 'https://placeimg.com/64/64/1',
+      'name': '엄세용',
+      'birthday': '850409',
+      'gender': '남자',
+      'job': 'CEO'
+    },
+    {
+      'id': 2,
+      'image': 'https://placeimg.com/64/64/2',
+      'name': '심수현',
+      'birthday': '870210',
+      'gender': '여자',
+      'job': '화가'
+    },
+    {
+      'id': 3,
+      'image': 'https://placeimg.com/64/64/3',
+      'name': '홍성학',
+      'birthday': '851210',
+      'gender': '남자',
+      'job': '자산설계사'
+    }
+    ]);  
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
